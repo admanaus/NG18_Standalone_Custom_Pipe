a200245677b1862483c8d3d3a8f24c9d03b15eef
i23332 <i23332@verisk.com>
2024-09-09 08:15:45 -0600

    Initial commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import {Component} from '@angular/core';
+import {NgFor} from '@angular/common';
+import {HeroDetailComponent} from '../hero-detail/hero-detail.component';
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
+
+@Component({
+  standalone: true,
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  imports: [HeroDetailComponent, NgFor],
+})
+export class HeroesComponent {
+  heroes = HEROES;
+
+  selectedHero!: Hero | undefined;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  onDone($event: string): void {
+    console.log("done with: ", $event);
+    this.selectedHero = undefined;
+  }
+
+}
