7ff4a09195c4673de605bce6a0d0f993f592b228
i23332 <i23332@verisk.com>
2024-09-10 08:17:39 -0600

    standalone custom pipe

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,18 +1,24 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {NgIf, UpperCasePipe} from '@angular/common';
+import {NgIf, UpperCasePipe, DatePipe} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {Hero} from '../hero';
+import { ExponentialStrengthPipe } from '../pipes/exponentialStrengthPipe';
+
 
 @Component({
   standalone: true,
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css'],
-  imports: [FormsModule, NgIf, UpperCasePipe],
+  imports: [FormsModule, NgIf, UpperCasePipe, DatePipe, ExponentialStrengthPipe]
 })
 export class HeroDetailComponent {
   @Input() hero?: Hero;
   @Output() done = new EventEmitter<string>();
+
+  birthday = new Date(1988, 3, 15); // April 15, 1988
+  power = 6;
+  factor = '2';
 
   constructor() { }
 
@@ -23,6 +29,5 @@ export class HeroDetailComponent {
     this.done.emit(this.hero?.name);
   }
 
-
-
 }
+
